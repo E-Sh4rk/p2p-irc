5c12732a513b0c2329bd90c223d9b6baa155e012
Mickaël LAURENT <esh4rk@gmail.com>
2017-10-25 21:40:51 +0200

    update

--- p2p_irc/Chat.cs
+++ p2p_irc/Chat.cs
@@ -1,17 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
 namespace p2p_irc
 {
+	public struct MessageIdentifier
+	{
+		public ulong sender;
+		public uint nonce;
+	}
+
+	public struct NeighborInondationInfo
+	{
+		public PeerAddress neighbor;
+		public int numberAttempts;
+	}
+
+	public struct MessageInondationInfo
+	{
+		public Stopwatch timeElapsed;
+		public List<NeighborInondationInfo> neighbors;
+	}
+
 	public class Chat
 	{
 		TLV_utils tlv_utils;
 		Communications com;
 		Messages messages;
 
+		Dictionary<MessageIdentifier, MessageInondationInfo> inondationInfos;
+
 		public Chat(Communications com, TLV_utils tlv_utils, Messages messages)
 		{
 			this.com = com;
 			this.messages = messages;
 			this.tlv_utils = tlv_utils;
+			inondationInfos = new Dictionary<MessageIdentifier, MessageInondationInfo>();
 		}
 	}
 }
